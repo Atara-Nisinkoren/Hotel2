1c0dede6e96d63719509a0898fcfbae949d267d7
Noa-Swimmer <netanoas@gmail.com>
2021-01-03 18:17:39 +0200

    Pass Order to Payment

--- HotelProject/Controllers/HomeController.cs
+++ HotelProject/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace HotelProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly HotelProjectContext _context;
+        private DateTime _fromDate;
+        private DateTime _toDate;
+        private int _nunOfAdults;
+        private int _nunOfKids;
+        private int _nunOfInfants;
 
         public HomeController(ILogger<HomeController> logger, HotelProjectContext context)
         {
@@ -47,7 +52,13 @@ namespace HotelProject.Controllers
             //    ViewBag.EnterDetails = "You must specify dates and number of people!";
             //    return View("Index");
             //}
+            _fromDate = fromDate;
+            _toDate = toDate;
+            _nunOfAdults = numOfAdults;
+            _nunOfKids = numOfKids;
+            _nunOfInfants = numOfInfants;
             IEnumerable<Room> avaliableRooms = new List<Room>();
+            IEnumerable<RoomType> avaliableRoomsTypes = new List<RoomType>();
             List<int> notAvaliableRoomsIds = new List<int>();
             //take all rooms that appear in order table in the period given
             var ordersInSameDates =  _context.Order.Where(o => (o.FromDate >= fromDate && o.FromDate < toDate) || (o.ToDate > fromDate && o.FromDate < fromDate));
@@ -70,20 +81,35 @@ namespace HotelProject.Controllers
             }
             else
             {
-                avaliableRooms = _context.Room.ToList();
+                avaliableRoomsTypes = _context.RoomType.ToList();
             }
-            if(avaliableRooms.Count() > 0)
+            if(avaliableRoomsTypes.Count() > 0)
             {
-                return View("SearchResults",avaliableRooms);
+                return View("SearchResults", avaliableRoomsTypes);
             }
             ViewBag.NoRoomFound = "Sorry, there are no avaliable rooms for the dates specified.";
             return View("Index");
         }
-        //[HttpPost]
-        //public IActionResult Search()
-        //{
-        //    RedirectToAction("","",);
-        //}
+        public IActionResult RedirectToPayment(int typeId)
+        {
+            var chosenTypeRoom = _context.RoomType.FirstOrDefault(r => r.Id == typeId);
+            var perihod = _toDate - _fromDate;
+            if(chosenTypeRoom != null)
+            {
+                Order newOrder = new Order()
+                {
+                    FromDate = _fromDate,
+                    ToDate = _toDate,
+                    NumOfAdults = _nunOfAdults,
+                    NumOfKids = _nunOfKids,
+                    NumOfInfants = _nunOfKids,
+                    TotalPrice = chosenTypeRoom.BasicPrice * perihod.Days + _nunOfKids * 100 * perihod.Days,
+
+            };
+                return RedirectToAction("Payment", "Orders", newOrder);
+            }
+            return View("");
+        }
         public IActionResult Privacy()
         {
             return View();
